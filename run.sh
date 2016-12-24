0cbcb19958e160d5e193a67bbc45068bd309d38f
gorbynet <michael.roberts@iop.org>
2016-12-24 09:12:59 +0000

    Updated run script to get & import 4store data

--- run.sh
+++ run.sh
@@ -6,7 +6,9 @@ if [ ! -f /init-done ]; then
 
     # Create default store
     4s-backend-setup default
-
+    4s-backend default
+	4s-import default -fturtle -mhttp://data.iop.org/uat_review metadata.nt
+	4s-import default -fturtle -mhttp://data.iop.org/thesaurus/2016R3 thesaurus.rdf
     touch /init-done
 fi
 
